1f43f5eb9284a9da018bc48833217ec7570cd152
Richard-Walter <willthisnamedo@gmail.com>
2022-03-19 11:01:14 +1100

    added pinia example store

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,4 +1,4 @@
-
+<!-- 
 <template>
   <q-img class="" src="images\banner\about_us.jpg" alt="" />
 </template>
@@ -14,4 +14,27 @@ const {flask_data, error, load} = getFlaskData('greeting')
 
 load()
 
+</script> -->
+
+<template>
+  <div>
+    <p>hello</p>
+    <q-toggle v-model="store.counter" />
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useCounterStore } from '../store/example-store.js'
+
+export default {
+  setup () {
+    const store = useCounterStore()
+
+    return {
+      // you can return the whole store instance to use it in the template
+      store
+    }
+  }
+}
 </script>
